224db3296cbbf23b313848c07168e1201c94c0fd
shubham gupta <65035333+shu2496@users.noreply.github.com>
2021-02-23 16:06:20 +0530

    Update auth.js

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,14 +1,30 @@
 var express = require("express");
 var router = express.Router();
-const { signout, signup } = require("../controllers/auth"); //importing signout method from controller file to route
+const { check, validationResult } = require('express-validator');
+const { signout, signup,signin } = require("../controllers/auth"); //importing signout method from controller file to route
 
 
-router.post("/signup", signup);
+router.post(
+    "/signup",
+[
+    check("name" ,"name should be atleast 2 characters").isLength({ min: 3 }),
+    check("email" ,"email is required").isEmail(),
+    check("password" ,"password should be atleast 3 characters").isLength({ min: 3 })
+],
+ signup
+ );
+
+ router.post(
+     "/signin",
+[
+    
+    check("email" ,"email is required").isEmail(),
+    check("password" ,"password field is required").isLength({ min: 1 })
+],
+ signin
+ );
+
+
 router.get("/signout", signout);
 
 module.exports = router;
-
-
-
-
-
