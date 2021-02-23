21d8b7ce87df352a175cfd6802772e5f338c68ef
shubham gupta <65035333+shu2496@users.noreply.github.com>
2021-02-23 16:44:56 +0530

    Update auth.js

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -70,7 +70,37 @@ exports.signin = (req, res) => {
 };
 
 exports.signout = (req, res) => {
+  res.clearCookie("token");
   res.json({
-    message: 'User signout',
+    message: 'User signout successfully',
   });
 };
+
+// protected routes
+exports.isSignedIn = exppressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth"
+});
+
+// custom middlewares
+exports.isAuthenticated = (req, res, next) =>{
+  let  checker = req.profile && req.auth && req.profile._id ===req.auth._id;
+  if(!checker){
+    return res.status(403).json({
+      error: "ACCESS DENIED"
+    });  
+  }
+  next();
+}
+
+exports.isAdmin = (req, res, next) =>{
+  if(req.profile.role === 0){
+    return res.status(403).json({
+      error: "YOU ARE NOT ADMIN, ACCESS DENIED"
+    });
+  }
+  next();
+}
+
+
+
